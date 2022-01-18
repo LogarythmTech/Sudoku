0ba502e613e5250df0cf6379b6ad907c150dcb42
Logan Richards <sdrahcir.log@me.com>
2022-01-17 17:49:47 -0700

    Created New Project

---
+++ Shared/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Shared
+//
+//  Created by Logan Richards on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
